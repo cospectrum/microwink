16b5f8e9ed323098c44a5a3649b319a4201790b8
Alex Severin <severinalexeyv@gmail.com>
2025-01-03 22:06:46 +0300

    add proptest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,11 @@
 import pytest
-import os
 
 from pathlib import Path
 from microwink import SegModel
 
-KiB = 2**10
-MiB = (2**10) * KiB
 
-
-@pytest.fixture
+@pytest.fixture(scope="package")
 def seg_model() -> SegModel:
     path = Path("./models/seg_model.onnx")
     assert path.exists()
-    size = os.path.getsize(path)
-    assert 10 * MiB < size < 15 * MiB
     return SegModel.from_path(path)
